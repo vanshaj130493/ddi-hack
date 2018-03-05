2b01d445aca51585dcfea8f6e4c09a537279de1f
Andy Brenneke <abrenneke@d2.com>
2018-03-05 13:44:41 -0800

    Basic server  that talks with Crate and Rethink

---
+++ index.js
@@ -0,0 +1,80 @@
+/**
+ * Hackathon example server
+ * Allows getting logs from CrateDB or RethinkDB using:
+ * HTTP GET /logs/cratedb?min=etc&max=etc
+ * or HTTP GET /logs/rethinkdb?min=etc&max=etc
+ *
+ * Feel free to modify this code however you want, or delete
+ * it and start over from scratch.
+ */
+
+require( 'dotenv/config' );
+const nconf = require( 'nconf' );
+const Koa = require( 'koa' );
+const Router = require( 'koa-router' );
+const crate = require( 'node-crate' );
+const rethinkdbdash = require( 'rethinkdbdash' );
+const moment = require( 'moment' );
+
+// Initialize configuration variables
+nconf.argv({ parseValues: true })
+    .env({ parseValues: true, lowerCase: true })
+    .defaults({
+        rethink_database: 'hackathon',
+        rethink_port: 28015,
+        crate_port: 4200,
+        app_port: 8080
+    })
+    .required([ 'rethink_database', 'rethink_host', 'rethink_port', 'crate_host', 'crate_port', 'app_port' ])
+
+// Connect to databases
+const r = rethinkdbdash({
+    db: nconf.get( 'rethink_database' ),
+    servers: [ { host: nconf.get( 'rethink_host' ), port: nconf.get( 'rethink_port' ) } ]
+});
+
+crate.connect( nconf.get( 'crate_host' ), nconf.get( 'crate_port' ) );
+
+// Start web server using Koa
+const app = new Koa();
+const router = new Router();
+
+// HTTP GET /logs/cratedb?min=etc&max=etc to get logs between dates
+router.get( '/logs/cratedb', async ctx => {
+    const { min, max } = ctx.query;
+    if ( !min || !max )
+        throw new Error( 'Must specify min and max in query string.' );
+
+    const minDate = moment.utc( min, moment.ISO_8601 ).toDate();
+    const maxDate = moment.utc( max, moment.ISO_8601 ).toDate();
+
+    const entries = await r.table( 'logs' ).filter( x => x( 'time' ).between( minDate, maxDate ) ).run();
+
+    ctx.status = 200;
+    ctx.body = entries;
+});
+
+// HTTP GET /logs/rethinkdb?min=etc&max=etc to get logs between dates
+router.get( '/logs/rethinkdb', async ctx => {
+    const { min, max } = ctx.query;
+    if ( !min || !max )
+        throw new Error( 'Must specify min and max in query string.' );
+
+    const minDate = moment.utc( min, moment.ISO_8601 ).toDate();
+    const maxDate = moment.utc( max, moment.ISO_8601 ).toDate();
+
+    const entries = await crate.execute( 'SELECT * FROM logs WHERE time BETWEEN ? AND ?', [ minDate, maxDate ]);
+
+    ctx.status = 200;
+    ctx.body = entries.json;
+});
+
+// Use router middleware
+app.use( router.routes() );
+app.use( router.allowedMethods() );
+
+// Start server on app port.
+const port = nconf.get( 'app_port' );
+app.listen( port, () => {
+    console.log( `Server started on port ${port}.` );
+});
