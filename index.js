4bdb0a0704f991ba7adc4ff93735795baed1aa85
Andy Brenneke <abrenneke@d2.com>
2018-03-06 23:54:09 -0800

    Run prettier on the code so most people will understand it

--- index.js
+++ index.js
@@ -8,17 +8,18 @@
  * it and start over from scratch.
  */
 
-require( 'dotenv/config' );
-const nconf = require( 'nconf' );
-const Koa = require( 'koa' );
-const Router = require( 'koa-router' );
-const crate = require( 'node-crate' );
-const logger = require( 'koa-logger' );
-const rethinkdbdash = require( 'rethinkdbdash' );
-const moment = require( 'moment' );
+require('dotenv/config');
+const nconf = require('nconf');
+const Koa = require('koa');
+const Router = require('koa-router');
+const crate = require('node-crate');
+const logger = require('koa-logger');
+const rethinkdbdash = require('rethinkdbdash');
+const moment = require('moment');
 
 // Initialize configuration variables
-nconf.argv({ parseValues: true })
+nconf
+    .argv({ parseValues: true })
     .env({ parseValues: true, lowerCase: true })
     .defaults({
         rethink_database: 'hackathon',
@@ -26,58 +27,73 @@ nconf.argv({ parseValues: true })
         crate_port: 4200,
         app_port: 8080
     })
-    .required([ 'rethink_database', 'rethink_host', 'rethink_port', 'crate_host', 'crate_port', 'app_port' ])
+    .required([
+        'rethink_database',
+        'rethink_host',
+        'rethink_port',
+        'crate_host',
+        'crate_port',
+        'app_port'
+    ]);
 
 // Connect to databases
 const r = rethinkdbdash({
-    db: nconf.get( 'rethink_database' ),
-    servers: [ { host: nconf.get( 'rethink_host' ), port: nconf.get( 'rethink_port' ) } ]
+    db: nconf.get('rethink_database'),
+    servers: [
+        { host: nconf.get('rethink_host'), port: nconf.get('rethink_port') }
+    ]
 });
 
-crate.connect( nconf.get( 'crate_host' ), nconf.get( 'crate_port' ) );
+crate.connect(nconf.get('crate_host'), nconf.get('crate_port'));
 
 // Start web server using Koa
 const app = new Koa();
 const router = new Router();
 
-app.use( logger() );
+app.use(logger());
 
 // HTTP GET /logs/cratedb?min=etc&max=etc to get logs between dates
-router.get( '/logs/cratedb', async ctx => {
+router.get('/logs/cratedb', async ctx => {
     const { min, max } = ctx.query;
-    if ( !min || !max )
-        throw new Error( 'Must specify min and max in query string.' );
+    if (!min || !max)
+        throw new Error('Must specify min and max in query string.');
 
-    const minDate = moment.utc( min, moment.ISO_8601 ).toDate();
-    const maxDate = moment.utc( max, moment.ISO_8601 ).toDate();
+    const minDate = moment.utc(min, moment.ISO_8601).toDate();
+    const maxDate = moment.utc(max, moment.ISO_8601).toDate();
 
-    const entries = await r.table( 'logs' ).filter( x => x( 'time' ).between( minDate, maxDate ) ).run();
+    const entries = await r
+        .table('logs')
+        .filter(x => x('time').between(minDate, maxDate))
+        .run();
 
     ctx.status = 200;
     ctx.body = entries;
 });
 
 // HTTP GET /logs/rethinkdb?min=etc&max=etc to get logs between dates
-router.get( '/logs/rethinkdb', async ctx => {
+router.get('/logs/rethinkdb', async ctx => {
     const { min, max } = ctx.query;
-    if ( !min || !max )
-        throw new Error( 'Must specify min and max in query string.' );
+    if (!min || !max)
+        throw new Error('Must specify min and max in query string.');
 
-    const minDate = moment.utc( min, moment.ISO_8601 ).toDate();
-    const maxDate = moment.utc( max, moment.ISO_8601 ).toDate();
+    const minDate = moment.utc(min, moment.ISO_8601).toDate();
+    const maxDate = moment.utc(max, moment.ISO_8601).toDate();
 
-    const entries = await crate.execute( 'SELECT * FROM logs WHERE time BETWEEN ? AND ?', [ minDate, maxDate ]);
+    const entries = await crate.execute(
+        'SELECT * FROM logs WHERE time BETWEEN ? AND ?',
+        [minDate, maxDate]
+    );
 
     ctx.status = 200;
     ctx.body = entries.json;
 });
 
 // Use router middleware
-app.use( router.routes() );
-app.use( router.allowedMethods() );
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 // Start server on app port.
-const port = nconf.get( 'app_port' );
-app.listen( port, () => {
-    console.log( `Server started on port ${port}.` );
+const port = nconf.get('app_port');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
 });
